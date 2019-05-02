e21ec65b468f27abedc552dc0e9952736700089f
ben <benhuang1024@gmail.com>
2019-05-02 22:04:59 +0800

    增加配置项功能

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Cornerstone
 
 用于 clone 的项目基础
+
+1.配置待克隆项目
+```php
+$config['wait_capture_urls']  // 待克隆 urls
+$config['is_deep']  // 深克隆，进行本地化资源
+$config['deep_type']  // 本地化资源类型（图片，js，css）
+```
+
+
+2.运行方式（直接运行 /index.php）
+```
+php index.php
+
+or
+
+php -S localhost:8000
+chrome open localhost:8000
+```
+
+3.在运行结束后，复制 response 至 Laravel 项目中即可
+
+<hr>
+Tips:  
+
+如果进程卡死，请释放端口 9515
+```
+lsof -i:9515
+kill PID
+```
--- index.php
+++ index.php
@@ -1,14 +1,24 @@
 <?php
 require __DIR__ . '/vendor/autoload.php'; // Composer's autoloader
 
-$client = \Symfony\Component\Panther\Client::createChromeClient();
-$crawler = $client->request('GET', 'https://api-platform.com'); // Yes, this website is 100% in JavaScript
+try {
+    $config = [
+        'wait_capture_urls' => [
+            'index'  => 'https://www.bilibili.com',
+            'list'   => 'https://www.bilibili.com/v/dance/',
+            'detail' => 'https://www.bilibili.com/video/av50530804/'
+        ]
+    ];
 
-$link = $crawler->selectLink('Support')->link();
-$crawler = $client->click($link);
-
-// Wait for an element to be rendered
-$client->waitFor('.support');
-
-echo $crawler->filter('.support')->text();
-//$client->takeScreenshot('screen.png'); // Yeah, screenshot!
+    exec('rm -rf response');
+    exec('cp -a response_temp response');
+    $client = \Symfony\Component\Panther\Client::createChromeClient();
+    foreach ($config['wait_capture_urls'] as $capture_name => $capture_url) {
+        $crawler = $client->request('GET', $capture_url);
+        $file_name = 'response/resources/views/' . $capture_name . '.blade.php';
+        file_put_contents($file_name, $crawler->html());
+    }
+    echo 'success,you need clone response blade files';
+} catch (Exception $exception) {
+    echo $exception->getCode() . $exception->getMessage();
+}
